b47ed3a3258d3fed4d179394e9843698fa76e60a
高炳炳 <1019178691@qq.com>
2019-03-05 11:00:15 +0800

    桌面应用初始化

---
+++ main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow} = require('electron');
+const env = process.argv[3];
+
+// 浏览器引用
+let mainWindow;
+
+// 创建浏览器窗口函数
+let createWindow = () => {
+  // 创建浏览器窗口
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  if(env === "dev") { 
+    mainWindow.loadURL("http://localhost:3000/")
+  } else if (env === "bvuild") { 
+    mainWindow.loadFile('./build/index.html/');
+  }
+
+  //打开开发者工具
+  mainWindow.webContents.openDevTools({mode:'bottom'});
+
+  // 当window被关闭时，除掉window的引用
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+};
+
+// 当app准备就绪时候开启窗口
+app.on('ready', createWindow);
+
+//当全部窗口都被关闭之后推出
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// 在macos上，单击dock图标并且没有其他窗口打开的时候，重新创建一个窗口
+app.on('activate', () => {
+  if (window == null) {
+    createWindow();
+  }
+});
