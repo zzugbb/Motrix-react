ce1c2ceb63fe36ae381b4804c0322f5ad3240f90
高炳炳 <1019178691@qq.com>
2019-03-05 18:32:41 +0800

    补充

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Icon } from 'antd';
 import LeftMenu from '../../components/LeftMenu'
 import TopMenu from '../../components/TopMenu'
 import styles from './index.module.css';
@@ -6,11 +7,30 @@ import styles from './index.module.css';
 class Home extends Component {
   render() {
     return (
-      <div >
+      <div style={{display: "flex"}}>
         <LeftMenu/>
         <div className={styles.subnav}>
+          <p>任务列表</p>
+          <ul className={styles.taskUl}>
+            <li className={styles.taskActiveLi}>
+              <Icon type="download" />
+              <span>下载中</span>
+            </li>
+            <li>
+              <Icon type="pause" />
+              <span>等待中</span>
+            </li>
+            <li>
+              <Icon type="stop" />
+              <span>已停止</span>
+            </li>
+          </ul>
+        </div>
+        <div style={{flex: 1}}>
           <TopMenu/>
-          1111
+          <div>
+            222
+          </div>
         </div>
       </div>
     );
